70ba6d0bad619c57e7d2294a1dd330c06fe4db25
Jon McPherson <jon@jonmcpherson.com>
2019-03-02 13:40:21 -0800

    Add docs to GeoPoint class

--- OpenLocationCode/GeoPoint.cs
+++ OpenLocationCode/GeoPoint.cs
@@ -1,7 +1,14 @@
 using System;
 namespace Google.OpenLocationCode {
+    /// <summary>
+    /// A point on the three-dimensional geographic coordinate system specified by latitude and longigtude coordinates in degrees.
+    /// </summary>
     public struct GeoPoint : IEquatable<GeoPoint> {
 
+        /// <param name="latitude">The latitude coodinate in degrees</param>
+        /// <param name="longitude">The longitude coordinate in degrees</param>
+        /// <exception cref="ArgumentException">If latitude is out of range -90 to 90</exception>
+        /// <exception cref="ArgumentException">If longitude is out of range -180 to 180</exception>
         public GeoPoint(double latitude, double longitude) {
             if (latitude < -90 || latitude > 90) throw new ArgumentException("latitude is out of range -90 to 90");
             if (longitude < -180 || longitude > 180) throw new ArgumentException("longitude is out of range -180 to 180");
@@ -10,11 +17,18 @@ namespace Google.OpenLocationCode {
             Longitude = longitude;
         }
 
+        /// <summary>
+        /// The latitude coordinate in degrees (y axis)
+        /// </summary>
         public double Latitude { get; }
 
+        /// <summary>
+        /// The longitude coordinate in degrees (x axis)
+        /// </summary>
         public double Longitude { get; }
 
 
+        /// <returns>A human readable representation of this GeoPoint</returns>
         public override string ToString() => $"[Longitude:{Longitude},Latitude:{Latitude}]";
 
         public override int GetHashCode() => Longitude.GetHashCode() ^ Latitude.GetHashCode();

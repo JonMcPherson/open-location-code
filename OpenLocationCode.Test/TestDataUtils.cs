879aed57f615ac41e36ff141284dc89a0b97cac2
Biko Baingaru <bikobaingaru@outlook.com>
2021-08-28 10:50:47 +0300

    Upgrade to netstandard2.1

--- OpenLocationCode.Test/TestDataUtils.cs
+++ OpenLocationCode.Test/TestDataUtils.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections.Immutable;
+using System.Globalization;
 using System.IO;
 using CsvHelper;
 using CsvHelper.Configuration;
 
 
-public static class TestDataUtils {
+public static class TestDataUtils
+{
 
-    public static ImmutableList<T> ReadTestData<T>(string fileName) {
+    public static ImmutableList<T> ReadTestData<T> ( string fileName )
+    {
         var relativePath = Path.Combine("..", "..", "..", "TestData", fileName);
-        using (var reader = new StreamReader(relativePath)) {
-            using (var csv = new CsvReader(reader, new Configuration { HasHeaderRecord = false, AllowComments = true })) {
+        using (var reader = new StreamReader(relativePath))
+        {
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false, AllowComments = true }))
+            {
                 return csv.GetRecords<T>().ToImmutableList();
             }
         }
